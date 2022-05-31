eb5a31b3971c942fbafba34bafad1648a825ab6c
David Revay <daverevay@gmail.com>
2022-05-31 15:58:39 +1000

    chore: fiddle

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -51,74 +51,74 @@ self: super: with self.lib; let
     });
   }));
 
-# in {
-#   colcon = with self.python3Packages; colcon-core.withExtensions [
-#     colcon-cmake
-#     colcon-core
-#     colcon-library-path
-#     colcon-metadata
-#     colcon-package-selection
-#     colcon-python-setup-py
-#     colcon-recursive-crawl
-#     colcon-ros
-#     colcon-test-result
-#   ];
+in {
+  # colcon = with self.python3Packages; colcon-core.withExtensions [
+  #   colcon-cmake
+  #   colcon-core
+  #   colcon-library-path
+  #   colcon-metadata
+  #   colcon-package-selection
+  #   colcon-python-setup-py
+  #   colcon-recursive-crawl
+  #   colcon-ros
+  #   colcon-test-result
+  # ];
 
-#   gazebo_9 = self.libsForQt5.callPackage ./gazebo/9.nix { };
-#   gazebo_11 = self.libsForQt5.callPackage ./gazebo { };
-#   gazebo = self.gazebo_11;
+  # gazebo_9 = self.libsForQt5.callPackage ./gazebo/9.nix { };
+  # gazebo_11 = self.libsForQt5.callPackage ./gazebo { };
+  # gazebo = self.gazebo_11;
 
-#   geographiclib = self.callPackage ./geographiclib { };
+  geographiclib = self.callPackage ./geographiclib { };
 
-#   ignition = {
-#     cmake0 = self.callPackage ./ignition/cmake/0.nix { };
-#     cmake2 = self.callPackage ./ignition/cmake { };
-#     cmake = self.ignition.cmake2;
+  # ignition = {
+  #   cmake0 = self.callPackage ./ignition/cmake/0.nix { };
+  #   cmake2 = self.callPackage ./ignition/cmake { };
+  #   cmake = self.ignition.cmake2;
 
-#     common1 = self.callPackage ./ignition/common/1.nix { };
-#     common3 = self.callPackage ./ignition/common/3.nix { };
-#     common4 = self.callPackage ./ignition/common { };
-#     common = self.ignition.common4;
+  #   common1 = self.callPackage ./ignition/common/1.nix { };
+  #   common3 = self.callPackage ./ignition/common/3.nix { };
+  #   common4 = self.callPackage ./ignition/common { };
+  #   common = self.ignition.common4;
 
-#     fuel-tools1 = self.callPackage ./ignition/fuel-tools/1.nix { };
-#     fuel-tools4 = self.callPackage ./ignition/fuel-tools/4.nix { };
-#     fuel-tools6 = self.callPackage ./ignition/fuel-tools { };
-#     fuel-tools = self.ignition.fuel-tools6;
+  #   fuel-tools1 = self.callPackage ./ignition/fuel-tools/1.nix { };
+  #   fuel-tools4 = self.callPackage ./ignition/fuel-tools/4.nix { };
+  #   fuel-tools6 = self.callPackage ./ignition/fuel-tools { };
+  #   fuel-tools = self.ignition.fuel-tools6;
 
-#     math4 = self.callPackage ./ignition/math/4.nix { };
-#     math6 = self.callPackage ./ignition/math { };
-#     math = self.ignition.math6;
+  #   math4 = self.callPackage ./ignition/math/4.nix { };
+  #   math6 = self.callPackage ./ignition/math { };
+  #   math = self.ignition.math6;
 
-#     msgs1 = self.callPackage ./ignition/msgs/1.nix { };
-#     msgs5 = self.callPackage ./ignition/msgs/5.nix { };
-#     msgs7 = self.callPackage ./ignition/msgs { };
-#     msgs = self.ignition.msgs7;
+  #   msgs1 = self.callPackage ./ignition/msgs/1.nix { };
+  #   msgs5 = self.callPackage ./ignition/msgs/5.nix { };
+  #   msgs7 = self.callPackage ./ignition/msgs { };
+  #   msgs = self.ignition.msgs7;
 
-#     tools = self.callPackage ./ignition/tools { };
+  #   tools = self.callPackage ./ignition/tools { };
 
-#     transport4 = self.callPackage ./ignition/transport/4.nix { };
-#     transport8 = self.callPackage ./ignition/transport/8.nix { };
-#     transport10 = self.callPackage ./ignition/transport { };
-#     transport = self.ignition.transport10;
+  #   transport4 = self.callPackage ./ignition/transport/4.nix { };
+  #   transport8 = self.callPackage ./ignition/transport/8.nix { };
+  #   transport10 = self.callPackage ./ignition/transport { };
+  #   transport = self.ignition.transport10;
 
-#     utils1 = self.callPackage ./ignition/utils { };
-#     utils = self.ignition.utils1;
-#   };
+  #   utils1 = self.callPackage ./ignition/utils { };
+  #   utils = self.ignition.utils1;
+  # };
 
-#   openni2 = self.callPackage ./openni2 { };
+  # openni2 = self.callPackage ./openni2 { };
 
-#   opensplice_6_9 = self.callPackage ./opensplice { };
-#   opensplice = self.opensplice_6_9;
+  # opensplice_6_9 = self.callPackage ./opensplice { };
+  # opensplice = self.opensplice_6_9;
 
-#   python27 = pythonOverridesFor super.python27;
-#   python37 = pythonOverridesFor super.python37;
-#   python38 = pythonOverridesFor super.python38;
-#   python39 = pythonOverridesFor super.python39;
-#   python310 = pythonOverridesFor super.python310;
+  python27 = pythonOverridesFor super.python27;
+  python37 = pythonOverridesFor super.python37;
+  python38 = pythonOverridesFor super.python38;
+  python39 = pythonOverridesFor super.python39;
+  python310 = pythonOverridesFor super.python310;
 
-#   sdformat_6 = self.callPackage ./sdformat/6.nix { };
-#   sdformat_9 = self.callPackage ./sdformat { };
-#   sdformat = self.sdformat_9;
+  sdformat_6 = self.callPackage ./sdformat/6.nix { };
+  sdformat_9 = self.callPackage ./sdformat { };
+  sdformat = self.sdformat_9;
 
-#   superflore = self.python3Packages.callPackage ./superflore { };
-# }
+  superflore = self.python3Packages.callPackage ./superflore { };
+}
