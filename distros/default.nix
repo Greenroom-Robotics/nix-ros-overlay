86b981e133e19f790658f2d07c416e240af8ecce
David Revay <daverevay@gmail.com>
2022-05-31 16:08:56 +1000

    chore: fiddle

--- distros/default.nix
+++ distros/default.nix
@@ -4,26 +4,6 @@ self: super: {
   rosPackages = rec {
     lib = super.lib // import ../lib { inherit lib self; };
 
-    melodic = import ./distro-overlay.nix {
-      distro = "melodic";
-      python = self.python2;
-    } self super;
-
-    melodicPython3 = import ./distro-overlay.nix {
-      distro = "melodic";
-      python = self.python3;
-    } self super;
-
-    noetic = import ./distro-overlay.nix {
-      distro = "noetic";
-      python = self.python3;
-    } self super;
-
-    foxy = import ./distro-overlay.nix {
-      distro = "foxy";
-      python = self.python3;
-    } self super;
-
     galactic = import ./distro-overlay.nix {
       distro = "galactic";
       python = self.python3;
